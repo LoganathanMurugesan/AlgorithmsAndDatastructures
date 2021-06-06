853580ef36cddf5e8f9d7c763edacd9581dba2f3
Loganathan M <Loganathan_M3@Dellteam.com>
2021-06-06 18:13:55 +0530

    Bubbble sort with xunit test case

---
+++ AlgorithmsAndDatastructures.UnitTest/SortingTests.cs
@@ -0,0 +1,57 @@
+﻿using BubbleSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AlgorithmsAndDatastructures.UnitTest
+{
+
+    public class SortingTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        public SortingTests(ITestOutputHelper testOutoutHelper)
+        {
+            _testOutputHelper = testOutoutHelper;
+        }
+        [Fact]
+        public void BubbleSort_WhenGivenValidInput_SortsTheInput()
+        {
+            RunTestsForSortingAlgorithm(Sorting.BubbleSort);
+        }
+
+        private void RunTestsForSortingAlgorithm(Action<int[]> sort)
+        {
+            foreach (var sample in Samples())
+            {
+                sort(sample);
+                PrintOut(sample);
+            }
+        }
+
+        private void PrintOut(int[] array)
+        {
+
+            _testOutputHelper.WriteLine("----------TRACE----------\n");
+
+            foreach (var el in array)
+            {
+                _testOutputHelper.WriteLine(el + " ");
+            }
+
+            _testOutputHelper.WriteLine("\n-------------------------\n");             
+        }
+
+        private int[][] Samples()
+        {
+            int[][] samples = new int[5][];
+            samples[0] = new[] { 1 };
+            samples[1] = new[] { 2,1 };
+            samples[2] = new[] { 3,-1,5,8,2,9 };
+            samples[3] = new[] { 3,1,-9,7,8,4,3,2,6 };
+            samples[4] = new[] { 8, 2, 9, 1, -9, 7,-4,5 };
+
+            return samples;
+        }
+    }
+}
---
+++ BubbleSort/Sorting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    public class Sorting
+    {
+        public static void BubbleSort(int[] array)
+        {
+            for (int partIndex = array.Length - 1; partIndex > 0; partIndex--)
+            {
+                for (int i = 0; i < partIndex; i++)
+                {
+                    if(array[i] > array[i+1])
+                    {
+                        Swap(array, i, i + 1);
+                    }
+                }
+            }
+        }
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j) return;
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+    }
+}
